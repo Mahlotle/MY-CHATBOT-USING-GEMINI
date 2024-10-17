73e1caa8eda03367a8fe288747d8d697e29672c0
Mahlotle <137900766+Mahlotle@users.noreply.github.com>
2024-10-17 10:42:16 +0200

    Initial commit

---
+++ src/Context/Context.jsx
@@ -0,0 +1,93 @@
+import { createContext, useState } from "react";
+import PropTypes from 'prop-types';
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompts, setPrevPrompts] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
+
+    const delayPara = (index, nextWord) => {
+        const timer = setTimeout(() => {
+            setResultData(prev => prev + nextWord);
+        }, 75 * index);
+
+        return () => clearTimeout(timer); // Cleanup function
+    };
+
+    const newChat = () => {
+        setLoading(false);
+        setShowResult(false);
+    };
+
+    const onSent = async (prompt) => {
+        setResultData("");
+        setLoading(true);
+        setShowResult(true);
+        let response;
+
+        try {
+            if (prompt !== undefined) {
+                response = await run(prompt);
+                setRecentPrompt(prompt);
+            } else {
+                setRecentPrompt(input);
+                setPrevPrompts(prev => [...prev, input]);
+                response = await run(input);
+            }
+        } catch (error) {
+            console.error("Error fetching response:", error);
+            setLoading(false);
+            return; // Early return on error
+        }
+
+        let responseArray = response.split("**");
+        let newResponse = "";
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i % 2 !== 1) {
+                newResponse += responseArray[i];
+            } else {
+                newResponse += "<b>" + responseArray[i] + "</b>";
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("<br/>");
+        let newResponseArray = newResponse2.split(" ");
+        for (let i = 0; i < newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i];
+            delayPara(i, nextWord + " ");
+        }
+        setLoading(false);
+        setInput("");
+    };
+
+    const contextValue = {
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput,
+        newChat,
+    };
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    );
+};
+
+ContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default ContextProvider;
